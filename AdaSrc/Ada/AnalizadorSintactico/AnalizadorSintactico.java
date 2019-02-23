68d4c64eb1be448f6a81ca4ce9ff5b080e51848e
Federico Gomez Markowicz <fede.markoo@gmail.com>
2019-02-23 18:02:25 -0300

    Correcto

--- AdaSrc/Ada/AnalizadorSintactico/AnalizadorSintactico.java
+++ AdaSrc/Ada/AnalizadorSintactico/AnalizadorSintactico.java
@@ -2,7 +2,7 @@ package Ada.AnalizadorSintactico;
 
 import java.util.ArrayList;
 
-import BaseDeDatos.BDAda;
+import BaseDeDatos.BD;
 
 public class AnalizadorSintactico {
 
@@ -10,14 +10,14 @@ public class AnalizadorSintactico {
 
 	private static ArrayList<String> cargarCombinaciones() {
 		ArrayList<String> combo = new ArrayList<String>();
-		combo.addAll(BDAda.getCombinacionesSintactico());
+		combo.addAll(BD.getCombinacionesSintactico());
 		return combo;
 	}
 
 	public static Tipo analizar(String mensaje) {
 		combinacionesSintacticas = cargarCombinaciones();
 		for (String combinacion : combinacionesSintacticas) {
- 			Tipo retorno = new Tipo(combinacion, mensaje);
+			Tipo retorno = new Tipo(combinacion, mensaje);
 			if (retorno.match())
 				return retorno;
 		}
